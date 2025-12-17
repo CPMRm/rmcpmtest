b15606ca6bb29834a6964f777a4a054da78d6b65
CPMRm <rmcpmstore@gmail.com>
2025-12-18 02:37:04 +0800

    Update cpmtooldev.py

--- cpmtooldev.py
+++ cpmtooldev.py
@@ -7,7 +7,7 @@ import json
 
 __ENDPOINT_URL__: str = "cpmlan.squareweb.app"
 
-class CPMRm:
+class CPMTooldev:
     def __init__(self, access_key) -> None:
         self.auth_token = None
         self.access_key = access_key
