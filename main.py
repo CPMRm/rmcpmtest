69490eec37da7a091d30a6ab2191c021bb7a7dc3
CPMRm <rmcpmstore@gmail.com>
2025-12-18 00:23:16 +0800

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from rich.style import Style
 import pystyle
 from pystyle import Colors, Colorate
 
-from cpmrm import CPMRm
+from cpmtooldev import CPMRm
 
 __CHANNEL_USERNAME__ = "cpmrmstudio"
 __GROUP_USERNAME__   = "cpmrmstudiochat"
